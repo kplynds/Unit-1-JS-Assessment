2d221b2f278479438a30b39f530650f68cd1b977
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-09-22 14:22:21 -0700

    1-4-done

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ function getName(character) {
  * Sample data expected output: 5
  */
 function getFilmCount(character) {
-  // TODO: Add your code inside the functions (others below).
-
+  return character.films.length
 }
 
 /**
@@ -42,7 +41,12 @@ function getFilmCount(character) {
  * If length is 0. Return 'none'
 */
 function getSecondStarshipName(character) {
-  // TODO: Add your code here.
+  if (character.starships.length === 0) {
+    return "none";
+  }
+  else {
+  return character.starships[1].name
+  }
 }
 
 /**
@@ -55,7 +59,7 @@ function getSecondStarshipName(character) {
  *    Result: `Luke Skywalker, 172cm, 77kg. Featured in 5 films.`
  */
 function getSummary(character) {
-  // TODO: Add your code here.
+  return `${character.name}, ${character.height}cm, ${character.mass}kg. Featured in ${character.films.length} films.`
 }
 
 /**
